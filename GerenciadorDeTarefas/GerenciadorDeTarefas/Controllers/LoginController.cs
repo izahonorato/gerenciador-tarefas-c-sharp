0c03eeb9a83a63ee7d54da0bc9a5f596f2a16239
izahonorato <izahonorato@gmail.com>
2021-05-30 13:19:52 -0300

    tratamento de erro ao fazer login

--- GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GerenciadorDeTarefas.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -19,9 +21,20 @@ namespace GerenciadorDeTarefas.Controllers
         }
 
         [HttpPost]
-        public IActionResult EfetuarLogin()
+        public IActionResult EfetuarLogin([FromBody] LoginRequisiçaoDto requisicao)
         {
-
-        }
+            try
+            {
+                throw new ArgumentException("Dados obrigatórios não foram enviados");
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Ocorreu erro ao efetuar login", e, requisicao);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto() {
+                Status = StatusCodes.Status500InternalServerError,
+                Erro = "Ocorreu um erro ao efetuar login, tente novamente!"
+                });
+            }
+        }  
     }
 }
