ff08f095c1853d8e6b7b4fcef00ede39d330f245
Izaura <izahonorato@gmail.com>
2021-07-08 22:53:51 -0300

    Configuração do banco de dados

--- GerenciadorDeTarefas/GerenciadorDeTarefas/Startup.cs
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Startup.cs
@@ -1,8 +1,10 @@
+using GerenciadorDeTarefas.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,7 @@ namespace GerenciadorDeTarefas
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<GerenciadorDeTarefasContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
